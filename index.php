7adb826cf3c296c5002ed044b0105c47a0992912
fredy-svg <ferdii0704@gmail.com>
2024-11-30 21:59:14 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor magni dolores nihil at dolorum sint corrupti officiis porro sed placeat illum, laborum enim itaque molestiae voluptatem illo reprehenderit amet. A.s</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
